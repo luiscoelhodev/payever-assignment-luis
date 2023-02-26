08fc89bc886e3fcbefab08e12ff83f2d0303200c
Luis Coelho <mancoeluis@gmail.com>
2023-02-26 09:26:06 -0300

    test: add validation filter unit tests

---
+++ src/common/validation-exception.filter.spec.ts
@@ -0,0 +1,73 @@
+import { ValidationExceptionFilter } from './validation-exception.filter';
+import { ArgumentsHost, BadRequestException } from '@nestjs/common';
+
+describe('ValidationExceptionFilter', () => {
+  let filter: ValidationExceptionFilter;
+
+  beforeEach(() => {
+    filter = new ValidationExceptionFilter();
+  });
+
+  describe('catch', () => {
+    it('should set the response status code to 400', () => {
+      const exception = new BadRequestException('Validation failed');
+      const response = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const host = {
+        switchToHttp: jest.fn().mockReturnValue({
+          getResponse: () => response,
+        }),
+      };
+
+      filter.catch(exception, host as unknown as ArgumentsHost);
+
+      expect(response.status).toHaveBeenCalledWith(400);
+    });
+
+    it('should set the response body with the validation errors', () => {
+      const exception = new BadRequestException('Validation failed');
+      const response = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const host = {
+        switchToHttp: jest.fn().mockReturnValue({
+          getResponse: () => response,
+        }),
+      };
+      const errors = [{ field: 'name', message: 'Name is required' }];
+      exception.getResponse = jest.fn().mockReturnValue(errors);
+
+      filter.catch(exception, host as unknown as ArgumentsHost);
+
+      expect(response.json).toHaveBeenCalledWith({
+        statusCode: 400,
+        errors,
+      });
+    });
+
+    it('should set the response body with a single validation error', () => {
+      const exception = new BadRequestException('Validation failed');
+      const response = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const host = {
+        switchToHttp: jest.fn().mockReturnValue({
+          getResponse: () => response,
+        }),
+      };
+      const error = { field: 'name', message: 'Name is required' };
+      exception.getResponse = jest.fn().mockReturnValue(error);
+
+      filter.catch(exception, host as unknown as ArgumentsHost);
+
+      expect(response.json).toHaveBeenCalledWith({
+        statusCode: 400,
+        errors: [error],
+      });
+    });
+  });
+});
